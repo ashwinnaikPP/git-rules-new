1fb914cc8e2f329e508449acb04dee55ec214fc7
Ashwin Naik <ashwinnaik94@gmail.com>
2023-02-16 00:05:47 +0530

    First commit

---
+++ Documents/Automation/Ultralesson/capstone_api/src/test/java/users/UserClient.java
@@ -0,0 +1,12 @@
+package users;
+
+import org.testng.annotations.Test;
+
+public class UserClient {
+
+    @Test
+    public void testMethod(){
+        System.out.println("Simple Test");
+    }
+
+}
